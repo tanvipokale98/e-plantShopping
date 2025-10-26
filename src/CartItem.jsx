8b4de8cd38fb32affa5ae1f89b0893e617149b90
Tanvi <pokaletanvi095@gmail.com>
2025-10-26 13:43:57 -0400

    task completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -25,7 +25,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleCheckoutShopping = (e) => {
-    alert('Checkout functionality to be added for future reference.');
+    alert('Coming Soon.');
 };
 
 
@@ -77,7 +77,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
